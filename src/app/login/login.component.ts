ba275d56470f28fa89586b6fa2492e24004c176a
godsakani <nyonggodwill11@gmail.com>
2022-07-23 23:22:16 +0100

    feat: added login component

---
+++ src/app/login/login.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+import * as firebase from '@firebase/app';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss']
+})
+export class LoginComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+  login(){
+
+  }
+
+}
