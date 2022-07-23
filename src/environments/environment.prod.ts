dad3e94af9a9902e862ce6155c82b2cbed378fe8
godsakani <nyonggodwill11@gmail.com>
2022-07-23 23:54:16 +0100

    file: added environment for work

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase : {
+    apiKey: "API_KEY",
+    authDomain: "oshop-3e649.firebaseapp.com",
+    projectId: "oshop-3e649",
+    storageBucket: "oshop-3e649.appspot.com",
+    messagingSenderId: "209115482999",
+    appId: "1:209115482999:web:f60b021803d63c1ebe2f72"
+  }
 };
