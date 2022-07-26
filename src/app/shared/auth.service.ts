00110fb2ff7abfb50cfa2aaff6ea8080b94128d5
godsakani <nyonggodwill11@gmail.com>
2022-07-27 00:02:56 +0100

    service: added service component

---
+++ src/app/shared/auth.service.ts
@@ -0,0 +1,43 @@
+import { Injectable } from '@angular/core';
+import { GoogleAuthProvider } from 'firebase/auth';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
+import { Router } from "@angular/router";
+import * as firebase from 'firebase/compat/app';
+import { Observable } from 'rxjs';
+
+
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AuthService {
+  user$: Observable<firebase.default.User | null | undefined>; 
+  constructor(
+    private afAuth: AngularFireAuth , public router: Router // Inject Firebase auth service
+  ) {
+    this.user$ = afAuth.authState;
+  }
+  // Sign in with Google
+  GoogleAuth() {
+    return this.AuthLogin(new GoogleAuthProvider());
+  }
+  AuthLogin(provider: any) {
+    return this.afAuth
+      .signInWithPopup(provider)
+      .then((result) => {
+        this.router.navigate(['check-out']);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
+  // login(){
+  //   this.afAuth.signInWithRedirect( new firebase.default.auth.GoogleAuthProvider());
+  // }
+
+  logout(){
+    return this.afAuth.signOut().then(() =>{
+      this.router.navigate(['login']);
+    })
+  }
+}
