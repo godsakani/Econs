f8396fd7d79c5bfe7e7ecb9a2c77ad3799c49d49
godsakani <nyonggodwill11@gmail.com>
2022-07-27 00:06:14 +0100

    feat: inject services for auth

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,15 +1,24 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from "@angular/router";
+import { AuthService } from '../shared/auth.service';
+
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.scss']
 })
-export class NavbarComponent implements OnInit {
+export class NavbarComponent {
+ 
+  constructor( public auth: AuthService , public router: Router ) {
+ 
+  }
 
-  constructor() { }
+  // ngOnInit(): void {
+  // }
 
-  ngOnInit(): void {
+  logout(){
+    this.auth.logout();
   }
 
 }
