a17f556c7ecbca47bf26b0f4674a9591dd836288
godsakani <nyonggodwill11@gmail.com>
2022-07-27 00:04:35 +0100

    fear: modified environment file

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,11 +1,12 @@
 export const environment = {
   production: false,
   firebase: {
-    apiKey: "api Key from firebase",
-    authDomain: "oshop-3e649.firebaseapp.com",
-    projectId: "oshop-3e649",
-    storageBucket: "oshop-3e649.appspot.com",
-    messagingSenderId: "209115482999",
-    appId: "1:209115482999:web:f60b021803d63c1ebe2f72"
+  apiKey: "API_Key",
+  authDomain: "oshop-3e649.firebaseapp.com",
+  databaseURL: "https://oshop-3e649-default-rtdb.firebaseio.com",
+  projectId: "oshop-3e649",
+  storageBucket: "oshop-3e649.appspot.com",
+  messagingSenderId: "209115482999",
+  appId: "1:209115482999:web:f60b021803d63c1ebe2f72"
   }
 }; 
