3968605cb35ca7ca428c5198d21846ef8cd8a88e
godsakani <nyonggodwill11@gmail.com>
2022-07-28 23:46:48 +0100

    feat: injected auth to app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from './shared/auth.service';
+import { UserService } from './shared/user.service';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +9,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'Oshop';
+ constructor(private auth: AuthService, router: Router, private userData: UserService){
+     auth.user$.subscribe(user => {
+      if (user) {
+       userData.save(user);
+
+        let returnUrl = localStorage.getItem('returnUrl');
+        router.navigateByUrl(returnUrl!);
+      }
+     })
+ }
 }
